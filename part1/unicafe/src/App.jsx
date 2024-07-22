a53cc9c5ca04c43c10745c3248e42f21a8d64a70
elsonfreire <elsonfreirefilho@gmail.com>
2024-07-22 16:36:15 -0300

    Part 1.7

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const incrementGood = () => {
+    setGood(good + 1);
+  };
+
+  const incrementNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const incrementBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={incrementGood} text="good"></Button>
+      <Button onClick={incrementNeutral} text="neutral"></Button>
+      <Button onClick={incrementBad} text="bad"></Button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
+};
+
+export default App;
