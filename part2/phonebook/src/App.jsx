4ed2cd890594f75e9c4bbc779b6003b1ad29186a
elsonfreire <elsonfreirefilho@gmail.com>
2024-07-22 22:27:17 -0300

    Part 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -10,6 +10,15 @@ const App = () => {
 
   const handleAddName = (event) => {
     event.preventDefault();
+
+    if (
+      persons.filter((person) => {
+        person.name === newName;
+      })
+    ) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
     setPersons(
       persons.concat({
         name: newName,
