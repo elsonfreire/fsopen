96b72ecde6ba4d58ea602e3045d28a21580692e2
elsonfreire <elsonfreirefilho@gmail.com>
2024-07-23 21:54:36 -0300

    Part 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Filter = ({ filter, handleFilterChange }) => {
   return (
@@ -57,12 +58,7 @@ const Persons = ({ persons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [filter, setFilter] = useState("");
   const [filteredPersons, setFilteredPersons] = useState(persons);
@@ -70,15 +66,22 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
     setFilteredPersons(
       persons.filter((person) => {
-        return person.name
-          .toLowerCase()
-          .includes(event.target.value.toLowerCase());
+        return person.name.toLowerCase().includes(filter.toLowerCase());
       })
     );
+  }, [persons, filter]);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
   };
 
   const handleNameChange = (event) => {
@@ -92,18 +95,21 @@ const App = () => {
   const handleAddPerson = (event) => {
     event.preventDefault();
 
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+    };
+
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons(
-      persons.concat({
-        name: newName,
-        number: newNumber,
-      })
-    );
-    setNewName("");
-    setNewNumber("");
+
+    axios.post("http://localhost:3001/persons", newPerson).then((response) => {
+      setPersons(persons.concat(response.data));
+      setNewName("");
+      setNewNumber("");
+    });
   };
 
   return (
